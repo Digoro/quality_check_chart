a270053dabf45dfb60a0c9ad772202ba5e82dc34
shlee3 <shlee@satreci.com>
2016-12-23 17:31:31 +0900

    one chart

--- app.py
+++ app.py
@@ -8,111 +8,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    arr = [0] * 20
-    return render_template('index.html', arr=arr)
+    return render_template('index.html')
 
 
-@app.route('/live-data1')
-def live_data1():
-    return get_data()
-
-
-@app.route('/live-data2')
-def live_data2():
-    return get_data()
-
-
-@app.route('/live-data3')
-def live_data3():
-    return get_data()
-
-
-@app.route('/live-data4')
-def live_data4():
-    return get_data()
-
-
-@app.route('/live-data5')
-def live_data5():
-    return get_data()
-
-
-@app.route('/live-data6')
-def live_data6():
-    return get_data()
-
-
-@app.route('/live-data7')
-def live_data7():
-    return get_data()
-
-
-@app.route('/live-data8')
-def live_data8():
-    return get_data()
-
-
-@app.route('/live-data9')
-def live_data9():
-    return get_data()
-
-
-@app.route('/live-data10')
-def live_data10():
-    return get_data()
-
-
-@app.route('/live-data11')
-def live_data11():
-    return get_data()
-
-
-@app.route('/live-data12')
-def live_data12():
-    return get_data()
-
-
-@app.route('/live-data13')
-def live_data13():
-    return get_data()
-
-
-@app.route('/live-data14')
-def live_data14():
-    return get_data()
-
-
-@app.route('/live-data15')
-def live_data15():
-    return get_data()
-
-
-@app.route('/live-data16')
-def live_data16():
-    return get_data()
-
-
-@app.route('/live-data17')
-def live_data17():
-    return get_data()
-
-
-@app.route('/live-data18')
-def live_data18():
-    return get_data()
-
-
-@app.route('/live-data19')
-def live_data19():
-    return get_data()
-
-
-@app.route('/live-data20')
-def live_data20():
-    return get_data()
-
-
-def get_data():
+@app.route('/live-data')
+def live_data():
     data = [time() * 1000, random() * 100]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
--- static/qcc.js
+++ static/qcc.js
@@ -1,57 +1,13 @@
 $(document).ready(function() {
     $('#paginatedTable').DataTable();
-} );
+});
 
-for(var i=1; i<21; i++) {
-    window['chart'+i];
-}
-
+var chart;
 var minRate = 20;
 var maxRate = 80;
 
-var title = {text: ""}
-var xAxis = {
-    type: 'datetime',
-    tickPixelInterval: 150,
-    maxZoom: 20 * 1000,
-    labels: {
-        format: '{value:%Y-%m-%d %H:%m:%S}',
-        rotation: 45,
-        align: 'left'
-    }
-}
-var yAxis = {
-    minPadding: 0.2,
-    maxPadding: 0.2,
-    title: {
-        text: 'A 품질',
-        margin: 80
-    },
-    plotLines: [{
-            value: minRate,
-            color: '#2d9bd2',
-            dashStyle: 'shortdash',
-            width: 2,
-            label: {
-                text: '품질 최소값'
-            }
-        }, {
-            value: maxRate,
-            color: '#b64949',
-            dashStyle: 'shortdash',
-            width: 2,
-            label: {
-                text: '품질 최대값'
-            }
-        }]
-}
-var series = [{
-    name: 'A 품질',
-    data: []
-}]
-
-function addRow(id, criterion, quality, time, color){
-    var tBody = document.getElementById(id);
+function addRow(criterion, quality, time, color) {
+    var tBody = document.getElementById("tbody");
     var row = tBody.insertRow(0);
     row.insertCell(0).innerHTML = criterion;
     row.insertCell(1).innerHTML = time;
@@ -59,12 +15,82 @@ function addRow(id, criterion, quality, time, color){
     row.style.backgroundColor = color;
 }
 
-for(var i=1; i<21; i++){
-    window['addRow' + i] = new Function('criterion', 'quality', 'time', 'color', 'addRow("tbody'+i+'", criterion, quality, time, color)');
-    window['requestData' + i] = new Function('$.ajax({'+'url: "/live-data'+i+'",success: function(point) {var series = chart'+i+
-    '.series[0];var shift = series.data.length > 20;series.addPoint(point, true, shift);var time = moment(point[0]).format("YYYY-MM-DD HH:mm:ss");var quality = point[1];if(quality > maxRate){$("#heading'+i+
-    '").css("background-color", "#b64949");addRow'+i+'("over", quality, time, "#F2DEDE");} else if(quality < minRate){$("#heading'+i+
-    '").css("background-color", "#2d9bd2");addRow'+i+'("under", quality, time, "#D9EDF7");} else{$("#heading'+i+'").css("background-color", "#DFF0D8");}setTimeout(requestData'+i+', 800);},cache: false});');
-    window['drawChart' + i] = new Function('id', 'chart'+i+' = new Highcharts.Chart({chart: {renderTo: id, defaultSeriesType: "spline", events: {load: requestData'+i+'}}, title: title, xAxis: xAxis, yAxis: yAxis, series: series});');
-    window['drawChart' + i]($('#qcc'+i)[0]);
+function requestData() {
+    $.ajax({
+        url: "/live-data",
+        success: function(point) {
+            var series = chart.series[0];
+            var shift = series.data.length > 20;
+            series.addPoint(point, true, shift);
+            var time = moment(point[0]).format("YYYY-MM-DD HH:mm:ss");
+            var quality = point[1];
+            if (quality > maxRate) {
+                $("#heading").css("background-color", "#b64949");
+                addRow("over", quality, time, "#F2DEDE");
+            } else if (quality < minRate) {
+                $("#heading").css("background-color", "#2d9bd2");
+                addRow("under", quality, time, "#D9EDF7");
+            } else {
+                $("#heading").css("background-color", "#DFF0D8");
+            }
+            setTimeout(requestData, 800);
+        },
+        cache: false
+    });
 }
+
+function drawChart(id) {
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: id,
+            defaultSeriesType: "spline",
+            events: {
+                load: requestData
+            }
+        },
+        title: {
+            text: ""
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000,
+            labels: {
+                format: '{value:%Y-%m-%d %H:%m:%S}',
+                rotation: 45,
+                align: 'left'
+            }
+        },
+        yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            title: {
+                text: 'A 품질',
+                margin: 80
+            },
+            plotLines: [{
+                value: minRate,
+                color: '#2d9bd2',
+                dashStyle: 'shortdash',
+                width: 2,
+                label: {
+                    text: '품질 최소값'
+                }
+            }, {
+                value: maxRate,
+                color: '#b64949',
+                dashStyle: 'shortdash',
+                width: 2,
+                label: {
+                    text: '품질 최대값'
+                }
+            }]
+        },
+        series: [{
+            name: 'A 품질',
+            data: []
+        }]
+    });
+}
+
+drawChart($('#qcc')[0])
